3a56e0aca2d30c6fffd2cd93101b81582d09228e
simon gladman <sgladman@gmail.com>
2016-03-09 05:11:57 +0000

    Initial Commit

---
+++ Sweetcorn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sweetcorn
+//
+//  Created by Simon Gladman on 09/03/2016.
+//  Copyright © 2016 Simon Gladman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sweetcorn/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sweetcorn
+//
+//  Created by Simon Gladman on 09/03/2016.
+//  Copyright © 2016 Simon Gladman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
